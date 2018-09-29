bb01df09c24e2c201df4aea2d2cb59eb177547a4
Omar A <oazzat19@gmail.com>
2018-09-29 18:02:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,13 @@
 def line (line)
   if line.length == 0
-    puts "The line is currently empty."
+    statement = "The line is currently empty."
   else
-    print "The line is currently:"
+    statement = "The line is currently:"
     counter = 1
     line.each do |name|
-      print " #{counter}. #{name}"
+      statement += " #{counter}. #{name}"
     end
   end
+  puts statement
 end
 
